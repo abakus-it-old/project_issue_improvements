4c39c29db433db42fb9985313552f131f4628222
Valentin THIRION <valentinthirion@gmail.com>
2015-02-24 16:24:26 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,8 @@
+This modules adds some functionalities to the issues for AbAKUS. 
+
+It adds:
+	- Auto set assigned stage when a person is assigned
+	- A due date field
+	- Moved the project field on the upper side
+
+This module has been developed by Bernard Delhez, intern @ AbAKUS it-solutions, under the control of Valentin Thirion.
---
+++ __openerp__.py
@@ -0,0 +1,18 @@
+﻿{
+    'name': "AbAKUS issue improvements",
+    'version': '1.0',
+    'depends': ['project_issue'],
+    'author': "Bernard DELHEZ, AbAKUS it-solutions SARL",
+    'website': "http://www.abakusitsolutions.eu",
+    'category': 'Contract',
+    'description': """This modules adds some functionalities to the issues for AbAKUS. 
+
+It adds:
+	- Auto set assigned stage when a person is assigned
+	- A due date field
+	- Moved the project field on the upper side
+
+This module has been developed by Bernard Delhez, intern @ AbAKUS it-solutions, under the control of Valentin Thirion.""",
+    'data': ['project_issue_view.xml',],
+    'demo': [],
+}
