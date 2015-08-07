b405df0ba282ffe2cfa21634dc0edf5adc3f34f6
Bernard Delhez <bernarddelhez@gmail.com>
2015-08-07 14:11:52 +0200

    server actions fixes

--- README.md
+++ README.md
@@ -1,10 +1,4 @@
-Latest UPDATES 2015-04-13:
-    - Merging modules in this module: 
-        - project_issue_remove_star_widget
-        - project_issue_form
-    - New server action for email matching.
-
-This modules adds some functionalities to the issues for AbAKUS. 
+#This modules adds some functionalities to the issues for AbAKUS. 
 
 Functionalities:
 ---
@@ -20,3 +14,15 @@ Functionalities:
     - Replaces priority stars in kanban view by the priority name.
 
 This module has been developed by Bernard Delhez, intern @ AbAKUS it-solutions, under the control of Valentin Thirion.
+
+Latest UPDATES 2015-08-07:
+    - corrections in 2 server actions:
+        - contact is set now
+        - BL project check
+        - check partner contacts email then partner email
+
+UPDATES 2015-04-13:
+    - Merging modules in this module: 
+        - project_issue_remove_star_widget
+        - project_issue_form
+    - New server action for email matching.
--- __openerp__.py
+++ __openerp__.py
@@ -7,12 +7,6 @@
     'category': 'Contract',
     'description': 
     """
-    Latest UPDATES 2015-04-13:
-        - Merging modules in this module: 
-            - project_issue_remove_star_widget
-            - project_issue_form
-        - New server action for email matching.
-
     This modules adds some functionalities to the issues for AbAKUS. 
 
     Functionalities:
@@ -28,6 +22,18 @@
         - Replaces priority stars in kanban view by the priority name.
 
     This module has been developed by Bernard Delhez, intern @ AbAKUS it-solutions, under the control of Valentin Thirion.
+    
+    Latest UPDATES 2015-08-07:
+        - corrections in 2 server actions:
+            - contact is set now
+            - BL project check
+            - check partner contacts email then partner email
+
+    UPDATES 2015-04-13:
+        - Merging modules in this module: 
+            - project_issue_remove_star_widget
+            - project_issue_form
+        - New server action for email matching.
     """,
     'data': ['view/project_issue_view.xml','ir_actions_server_data.xml'],
 }
